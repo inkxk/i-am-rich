ec4cc9fc19d023ec3b02c0da8e2cdfdd263d056a
Potcharapol Nokyoo <64063112+inkxk@users.noreply.github.com>
2022-07-14 23:48:41 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
